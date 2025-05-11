66f5aff383825dce24fade16452fa8760ee2bc35
shaden <shaden.wasaidi@gmail.com>
2025-05-11 15:46:38 +0300

    Rename the app

---
+++ FattenTests/FattenTests.swift
@@ -0,0 +1,17 @@
+//
+//  QiyamTests.swift
+//  QiyamTests
+//
+//  Created by shaden  on 10/11/1446 AH.
+//
+
+import Testing
+@testable import Fatten
+
+struct FattenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ QiyamUITests/FattenUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  QiyamUITestsLaunchTests.swift
+//  QiyamUITests
+//
+//  Created by shaden  on 10/11/1446 AH.
+//
+
+import XCTest
+
+final class FattenUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
